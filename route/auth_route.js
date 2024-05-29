de2c80a0df61e5489c463954bae03cabd58a5b85
Kanlapapluek_Kowinthaseth <kanlapapluek@gmail.com>
2024-05-29 17:45:47 +0700

    verifyToken

--- route/auth_route.js
+++ route/auth_route.js
@@ -5,24 +5,6 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 const jwt = require('jsonwebtoken');
 
-router.use((req, res, next) => {
-    console.log('middleware');
-    next();
-});
-
-// //http://localhost:8080/auth/login
-// router.route('/login').post((req, res) => {
-//     Db.getUserByUsername(req).then((data) => {
-//         // console.log(data);      
-//         res.status(200).json({ data: data, message: 'Login success' });  // ส่ง http code 200 และแสดง data
-//         // message ในรูปแบบ json
-//     }).catch(err => {
-//         res.status(500).send({ error: err, message: 'Server Error ' }) // ถ้า error จะส่ง http code 500
-//         // และแสดง err, message ในรูปแบบ json
-//         console.log(err);
-//     });
-// })
-
 //http://localhost:8080/auth/login
 router.route('/login').post((req, res) => {
     let user = { ...req.body } //ส่ง req.body เป็นข้อมูล json เข้าไปยังตัวแปร ship
@@ -45,46 +27,4 @@ router.route('/login').post((req, res) => {
     });
 })
 
-// router.post('/login', (req, res, next) => {
-
-//     passport.authenticate('local', {session: false}, (err, user, info) => {
-//         if (err) return next(err)
-//         if(user) {
-//             const token = jwt.sign(user, 'your_jwt_secret')
-//             return res.json({user, token})
-//         } else {
-//             return res.status(422).json(info)
-//          }
-//     })(req, res, next);
-// });
-
-// passport.use(new LocalStrategy((username, password, done) => {
-//     Db.postUser(username, password).then((data) =>{
-//         if (data.output.code == 'success') //return data.codde กลับมาเป็น success
-//         {
-//             user = data.recordset[0]
-//             return done(null,user)
-//         }
-//         else //return data เป็น error
-//         {
-//             return done(null, false, { message: 'Invalid credentials.\n' });
-//         }
-//         // console.log(data);      
-//     }).catch(err => {
-//         done(error);
-//         // และแสดง err, message ในรูปแบบ json
-//         console.log(err);
-//     });
-// }))
-
-        //     authUser = (user, password, done) => {
-        //         let authenticated_user = { id: data.recordset[0].UserID, name: data.recordset[0].Username} 
-        //         return done (null, authenticated_user ) 
-        // }
-        // passport.use(new LocalStrategy (authUser))
-        // passport.serializeUser( (user, done) => { 
-        //     console.log(`--------> Serialize User`)
-        //     console.log(user)     
-        //     done(null, user.id)
-        // } )
 module.exports = router; 
